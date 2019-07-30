4621375d054d123b18fcb272c0da47ae0eb68929
Galo DeSalazar <gdesalazar@gmail.com>
2019-07-30 17:55:42 -0400

    Updated entry.jsx for heroku

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Root from './components/root';
-import configureStore from './store/store';
+// import Root from './components/root';
+// import configureStore from './store/store';
 
+document.addEventListener(DOMContentLoaded,()=>{
+    let root = document.getElementById("root")
+    ReactDOM.render(<h1>SURFCAMP</h1>, root)
+})
