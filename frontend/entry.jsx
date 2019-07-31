c657538adb6083aff5b947a18f4ee68478777fb9
Galo DeSalazar <gdesalazar@gmail.com>
2019-07-31 09:24:07 -0400

    Added root.jsx, store, root reducer, session_errors reducer, session reducer, and updated entry.jsx

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import Root from './components/root';
-// import configureStore from './store/store';
+import configureStore from './store/store';
 
 document.addEventListener(DOMContentLoaded,()=>{
+    let store = configureStore();
     let root = document.getElementById("root");
-    ReactDOM.render(<h1>SURFCAMP</h1>, root)
+    ReactDOM.render(<Root store={store}/>, root)
 })
---
+++ frontend/reducers/root_reducer.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import session from './session_reducer';
+import sessionErrors from './session_errors_reducer';
+
+const rootReducer = combineReducers({
+    session,
+    sessionErrors,
+});
+
+export default rootReducer;
