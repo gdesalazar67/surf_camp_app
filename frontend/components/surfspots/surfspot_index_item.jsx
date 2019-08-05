839c244aba18c8bbf4b21fe321cc0af314da0351
Galo DeSalazar <gdesalazar@gmail.com>
2019-08-04 22:02:58 -0400

    Added css to index  page

--- frontend/components/surfspots/surfspot_index_item.jsx
+++ frontend/components/surfspots/surfspot_index_item.jsx
@@ -6,10 +6,10 @@ import {Link} from "react-router-dom";
     return(
         <li className="surfspot-container">
             <Link to={`/surfspots/${surfspot.id}`}>
-                <img src="http://cdn.cnn.com/cnnnext/dam/assets/130624173927-50-surf-spots-rincon.jpg"/>
+                <img src="http://cdn.cnn.com/cnnnext/dam/assets/130624173927-50-surf-spots-rincon.jpg" className="photo"/>
                 <p className="surfspot-title">{`${surfspot.title}`}</p>
                 <p className="surfspot-description">{`${surfspot.description}`}</p>
-                <p className="surfspot-price">${`${surfspot.price}`}</p>
+                <p className="surfspot-price">${`${surfspot.price}`}/night</p>
             </Link>
         </li>
     )
