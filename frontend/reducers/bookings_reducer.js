2628fdd5339d2967284703a52dc16c6e6bb0bf5d
Galo DeSalazar <gdesalazar@gmail.com>
2019-08-06 10:37:00 -0400

    added bookings reducer

---
+++ frontend/reducers/bookings_reducer.js
@@ -0,0 +1,23 @@
+import {RECEIVE_ALL_BOOKINGS, RECEIVE_BOOKING, REMOVE_BOOKING}from "../actions/bookings_actions";
+import {merge} from 'lodash';
+
+export default bookingsReducer = (state = {}, action)=>{
+    Object.freeze(state);
+    let newState,
+    switch(action.type){
+        case RECEIVE_ALL_BOOKINGS:
+            newState = merge({}, state, action.bookings);
+            return newState;
+        case RECEIVE_BOOKING:
+            newState = merge({}, state, {[action.booking.id]: action.booking});
+            return newState;
+        case REMOVE_BOOKING:
+            newState = mergee({}, state);
+            newState.delete(action.id);
+            return newState 
+        default:
+            return state;
+    }
+}
+
+export default bookingsReducer;
