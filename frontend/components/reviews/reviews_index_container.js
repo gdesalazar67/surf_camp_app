9472577986c7dad1925b9e434a8c99dea21850fe
Galo DeSalazar <gdesalazar@gmail.com>
2019-08-12 10:43:40 -0400

    Added ReviewsIndex container

---
+++ frontend/components/reviews/reviews_index_container.js
@@ -0,0 +1,24 @@
+import {connect} from "react-redux";
+import ReviewsIndex from "./reviewsIndex";
+import {withRouter} from "react-router";
+import {fetchReview, fetchReviews, deleteReview} from "../../actions/reviews_actions"
+
+const msp = (state, ownProps)=>{
+    
+    return({
+        reviews: state.entities.reviews,
+        currentUser: state.session.id,
+
+    })
+
+}
+
+const msd = dispatach =>{
+    return({
+        fetchReviews: (id)=>dispatach(fetchReviews(id)),
+        fetchReview: (id)=>dispatach(fetchReview(id)),
+        deleteReview: (id)=> dispatach(deleteReview(id)) 
+    })
+}
+
+export default withRouter(connect(msp, msd)(ReviewsIndex));
