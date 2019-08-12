b9e224b5533bd6a25f87b1642eb085da52d30576
Galo DeSalazar <gdesalazar@gmail.com>
2019-08-11 21:19:12 -0400

    Added reviews_api_utl ajax request

---
+++ frontend/util/reviews_api_util.js
@@ -0,0 +1,46 @@
+export const fetchReviews = (id)=>{
+    return(
+        $.ajax({
+            method: "GET",
+            url: `api/surfspots/${id}/reviews`
+        })
+    )
+}
+
+export const fetchReview = (id)=>{
+    return(
+        $.ajax({
+            method: "GET",
+            url: `api/reviews/${id}`
+        })
+    )
+}
+
+export const updateReview = (review) => {
+    return (
+        $.ajax({
+            method: "PATCH",
+            url: `api/surfspots/${review.surfspot_id}/reviews/${review.id}`,
+            data: {review}
+        })
+    )
+}
+
+export const createReview = (review) => {
+    return (
+        $.ajax({
+            method: "POST",
+            url: `api/surfspots/${review.surfspot_id}/reviews`,
+            data: {review}
+        })
+    )
+}
+
+export const deleteReview = (id) =>{
+    return(
+        $.ajax({
+            method: "DELETE",
+            url: `api/reviews/${id}`
+        })
+    )
+}
