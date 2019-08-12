263844a7353dd8bb1062d046e17f37cef017678b
Galo DeSalazar <gdesalazar@gmail.com>
2019-08-11 22:20:37 -0400

    Added reviews errors reducer

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import bookings from "./bookings_errors_reducer";
-import session from "./session_errors_reducer"
+import session from "./session_errors_reducer";
+import reviews from "./reviews_errors_reducer"
 
 const errorsReducer = combineReducers({
     bookings,
     session,
+    reviews,
 });
 
 export default errorsReducer;
---
+++ frontend/reducers/reviews_reducer.js
@@ -0,0 +1,16 @@
+export {RECEIVE_ERRORS, RECEIVE_REVIEW} from "../actions/reviews_actions";
+export {merge} from "lodash";
+
+export default (state =[], action)=>{
+    Object.freeze(state);
+    let newState,
+    switch (action.type){
+        case RECEIVE_ERRORS:
+            newState = merge([], action.errors)
+            return newState;
+        case RECEIVE_REVIEW:
+            return [];
+        default:
+            return state;
+    }
+}
