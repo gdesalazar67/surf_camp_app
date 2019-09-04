88c43d1c41cf2a892dd2fb20492dcfc06d082011
Galo DeSalazar <gdesalazar@gmail.com>
2019-09-04 18:48:22 -0400

    Added markerand infowindow on mouse over and redirect to surfspot hen marker clicked

--- frontend/components/map/google_map.jsx
+++ frontend/components/map/google_map.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
+import {Redirect} from 'react-router-dom';
 
 const mapStyles = {
     width: "400px",
@@ -8,9 +9,45 @@ const mapStyles = {
 
 class GoogleMap extends React.Component {
 
+    constructor(props){
+        super(props);
+        this.state = {
+            showingInfoWindow: false,
+            activeMarker: {},
+            selectedPlace: {},
+            redirect: false,
+        }
+        this.onMarkerMouseON = this.onMarkerMouseON.bind(this);
+        this.onMouseOut = this.onMouseOut.bind(this);
+        this.markerClick = this.markerClick.bind(this);
+    }
+
+    onMarkerMouseON(props, marker, e){ 
+        this.setState({
+            selectedPlace: props,
+            activeMarker: marker,
+            showingInfoWindow: true,
+        })
+    }
+
+    onMouseOut(props){
+        this.setState({
+            showingInfoWindow: false,
+            activeMarker: null,
+        })
+    }
+
+    markerClick(){
+        debugger
+        this.setState({redirect: true})
+        
+    }
    
     
     render() {
+        if(this.state.redirect === true){
+          return <Redirect to='/'/>
+        }
 
         return (
             <div className="google-map">
@@ -19,7 +56,22 @@ class GoogleMap extends React.Component {
                     zoom={8}
                     style={mapStyles}
                     initialCenter={{ lat: 47.444, lng: -122.176 }}
-                />
+                > 
+                    <Marker
+                        onMouseover={this.onMarkerMouseON}
+                        name={'Kenyatta International Convention Centre'}
+                        onMouseout={this.onMouseOut}
+                        onClick={this.markerClick}
+                    />
+                    <InfoWindow
+                        marker={this.state.activeMarker}
+                        visible={this.state.showingInfoWindow}
+                    >
+                        <div>
+                            <h4>{this.state.selectedPlace.name}</h4>
+                        </div>
+                    </InfoWindow>
+                </Map>
             </div>
         )
     };
