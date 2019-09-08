8abe28cfd4440ba59d502420ec814d97000ddb62
Galo DeSalazar <gdesalazar@gmail.com>
2019-09-08 18:30:27 -0400

    refactored geocode api call

--- frontend/util/geoCoder_api_util.js
+++ frontend/util/geoCoder_api_util.js
@@ -1,7 +1,7 @@
 export const fetchGeoCode = address =>{
     return(
         $.ajax({
-            url:"https://maps.googleapis.com/maps/api/geocode/json?address=43 valley view ave randolph, nj&key=AIzaSyAl8TSkkovuXys1EcUJFeDpZVI823ubNRg",
+            url:`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyAl8TSkkovuXys1EcUJFeDpZVI823ubNRg`,
             method: "GET"
         })
     )
