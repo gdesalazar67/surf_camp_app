04d7eadccd8920b247901d7ed3700ff5f3f2c52e
Galo DeSalazar <gdesalazar@gmail.com>
2020-02-06 08:06:30 -0800

    Reversed reviews to have lastest review first, added text transform capitlize to multiple items

--- frontend/components/reviews/reviewsIndex.jsx
+++ frontend/components/reviews/reviewsIndex.jsx
@@ -45,7 +45,7 @@ class ReviewsIndex extends React.Component{
                 </div>
             </div>
         }else{
-            reviewlist = reviews.map(
+            reviewlist = reviews.reverse().map(
                 review => {
                     return (
                         <ReviewItem
